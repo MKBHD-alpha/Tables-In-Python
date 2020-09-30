5a20dddb486f20ac301538c829c3117a52e5b940
Debarghya Das <72141607+MKBHD-alpha@users.noreply.github.com>
2020-09-30 20:51:15 +0530

    Tables V1

---
+++ Tables.py
@@ -0,0 +1,7 @@
+a=int(input("Enter any number:-- "))
+for i in range(1,11):
+    # print(f"{a} x {i} = " + str(a*i)) or print(f"{a} x {i} = {a*i}")
+    c=a*i
+    print(f"{a} x {i} = {c}")
+    
+    
