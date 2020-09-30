e0427c7d326822a571607c3f5dddd5e4ee805225
Debarghya Das <72141607+MKBHD-alpha@users.noreply.github.com>
2020-10-01 00:37:03 +0530

    With While Loop
    Hey, I read your Code, it was awesome you used FOR LOOP
    here's my version using WHILE LOOP
    hope you'll like it

--- Tables.py
+++ Tables.py
@@ -1,7 +1,21 @@
+#USING FOOR LOOP
+
 a=int(input("Enter any number:-- "))
+print(f"Table of {a} is as follows:-\n")
 for i in range(1,11):
-    # print(f"{a} x {i} = " + str(a*i)) or print(f"{a} x {i} = {a*i}")
     c=a*i
     print(f"{a} x {i} = {c}")
+    
+    
+    
+ #USING WHILE LOOP
+
+a= int(input("Enter any Number:--"))
+print(f"Table of {a} is as follows:-\n")
+b=0
+while b<=10:
+    c=a*b
+    print(f"{a} x {b} = {c}")
+    b+=1
     
     
